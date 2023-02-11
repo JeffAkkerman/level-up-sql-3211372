989c57ea289747c2032354325ad7abdf1c4b4345
JeffAkkerman <115320320+JeffAkkerman@users.noreply.github.com>
2023-02-11 17:40:39 +0000

    Learning SQL

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -4,16 +4,55 @@
 -- the Quinoa Salmon Salad.
 
 CREATE TABLE CustomersFavorites (
-  CustomerID INTEGER,
+  CustomerID INTEGER PRIMARY KEY,
   OrderID INTEGER,
   DishID Integer
 );
 
+SELECT *, COUNT(DishID)
+FROM Orders
+JOIN OrdersDishes ON Orders.OrderID = OrdersDishes.OrderID
+GROUP BY CustomerID, DishID
+ORDER BY CustomerID, COUNT(DishID) DESC;
+
 -- Tried to create a new table above that would track all the dishes ordered by each customer to then find the max
 
--- Going to try joining tables instead and finding the max and inputting that to the Customers table
 
-SELECT Customers.FirstName, Customers.LastName, Customers.Email, Reservations.Date, Reservations.PartySize
+
+
+-- -- Going to try joining tables instead and finding the max and inputting that to the Customers table
+
+-- SELECT Customers.FirstName, Customers.LastName, Customers.Email, Reservations.Date, Reservations.PartySize
+-- FROM Customers
+-- JOIN Reservations ON Customers.CustomerID = Reservations.CustomerID
+-- WHERE Customers.Email = 'smac@kinetecoinc.com';
+
+-- Set Cleo Goldwater's favorite dish to
+-- the Quinoa Salmon Salad.
+
+SELECT * FROM Customers
+WHERE FirstName == 'Cleo' AND LastName == 'Goldwater';
+
+-- CustomerID = 42
+
+SELECT * FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad';
+
+-- DishID = 9
+
+UPDATE Customers
+SET FavoriteDish = 9
+WHERE CustomerID = 42;
+
+SELECT * FROM Customers
+WHERE CustomerID = 42;
+
+-- In one query below
+
+UPDATE Customers
+SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE CustomerID = 42;
+
+SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.Name
 FROM Customers
-JOIN Reservations ON Customers.CustomerID = Reservations.CustomerID
-WHERE Customers.Email = 'smac@kinetecoinc.com';
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID;
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,19 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+-- First and Last Name, email, sort by number of orders
+-- Use Customers and Orders tables
+
+SELECT COUNT(*), Customers.FirstName, Customers.LastName, Customers.Email
+FROM Orders
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Orders.CustomerID
+ORDER BY COUNT(*) DESC;
+
+
+SELECT COUNT(Orders.OrderID) as OrderCount, Customers.FirstName, Customers.LastName, Customers.Email
+FROM Orders
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Orders.CustomerID
+ORDER BY OrderCount DESC
+LIMIT 6;
