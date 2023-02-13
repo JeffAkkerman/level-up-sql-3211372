e881aeb12c1bb9ab58497fd7502f3b17f7c7461d
JeffAkkerman <115320320+JeffAkkerman@users.noreply.github.com>
2023-02-13 22:29:35 +0000

    Exercise 2-8

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,23 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+
+SELECT COUNT(*), Published
+FROM Books
+GROUP BY Published
+ORDER BY Published DESC;
+
+
+SELECT COUNT(DISTINCT Title), Published
+FROM Books
+GROUP BY Published
+ORDER BY Published DESC;
+
+
+SELECT COUNT(Loans.LoanID), Books.Title
+FROM Loans
+JOIN Books ON Loans.BookID=Books.BookID
+GROUP BY Books.Title
+ORDER BY COUNT(Loans.LoanID) DESC
+LIMIT 5;
